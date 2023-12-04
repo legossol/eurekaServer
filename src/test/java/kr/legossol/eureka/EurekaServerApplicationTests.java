fba00582c37e0ae853b00d3496bd895a10bcb741
haesol Jang <wkdgothf@gmail.com>
2023-12-04 21:57:01 +0900

    first commit

---
+++ src/test/java/kr/legossol/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.legossol.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
